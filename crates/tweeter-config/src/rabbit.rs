85eedc6d8a819783808ac4a1757e9478efee922d
Kyrylo Baibula <kirillbaybula@gmail.com>
2022-10-17 10:52:51 +0300

    Add rabbit channel mq to config crate

--- crates/tweeter-config/src/rabbit.rs
+++ crates/tweeter-config/src/rabbit.rs
@@ -1,3 +1,6 @@
+use lapin::options::QueueDeclareOptions;
+use lapin::protocol::channel;
+use lapin::types::FieldTable;
 use lapin::{Connection, ConnectionProperties};
 use serde::Deserialize;
 
@@ -21,5 +24,32 @@ impl Parseable for Rabbit {
         let connection = Connection::connect(self.uri.as_str(), options).await?;
 
         connection.create_channel().await
+    }
+}
+
+#[derive(Deserialize)]
+pub struct RabbitChannel {
+    #[serde(flatten)]
+    pub inner: Rabbit,
+    pub queue: String,
+}
+
+#[async_trait::async_trait]
+impl Parseable for RabbitChannel {
+    type Error = lapin::Error;
+    type Value = tweeter_mq::rabbit::RabbitChannel;
+
+    async fn parse(self) -> Result<Self::Value, Self::Error> {
+        let channel = self.inner.parse().await?;
+
+        let _queue = channel
+            .queue_declare(
+                &self.queue,
+                QueueDeclareOptions::default(),
+                FieldTable::default(),
+            )
+            .await?;
+
+        Ok(tweeter_mq::rabbit::RabbitChannel::new(self.queue, channel))
     }
 }
